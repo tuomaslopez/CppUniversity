2001633757727c2d6433ffae2f553fee1e0c66fb
tuomaslopez <112478934+tuomaslopez@users.noreply.github.com>
2024-09-05 20:04:53 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// the main function
+int main(){
+    int a;
+    std::cout << "Hello there!" << std::endl;
+
+    a = 12;
+    std::cout << "a=" << a << std::endl;
+
+    a = 13;
+    std::cout << "a=" << a << std::endl;
+    return 0;
+}
