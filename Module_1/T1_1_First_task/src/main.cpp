5a02d90fad9877381c027576ce043c683bc4d6df
Tuomas Lopez <tuomas.lopez@aalto.fi>
2024-09-08 18:25:51 +0300

    kopioinut ja korjannut

--- Module_1/T1_1_First_task/src/main.cpp
+++ Module_1/T1_1_First_task/src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
 // the main function
-int main(void)
-{
-    /* Copy the code after this comment. */
-        return 0;
+int main(){
+    int a;
+    std::cout << "Hello Single step 3 lines, and check the value of variable `a`.\nThen, single step 2 more lines, and again check the value of `a`.\nContinue execution till debugging is terminated (after `return 0;` line).\nSubmit your implementationthere!" << std::endl;
+    a = 12;
+    std::cout << "a=" << a << std::endl;
+
+    a = 13;
+    std::cout << "a=" << a << std::endl;
+    return 0;
 }
